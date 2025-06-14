e6082682d6dcc7703435afd0935c30a396a3da95
Vladyslav <vladyslav.skvortsov.w@gmail.com>
2025-06-14 12:39:03 +0300

    refactor: script

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -5,14 +5,9 @@ document.addEventListener("click", (e) => {
 	const body = document.body;
 
 	if (targetElement.closest(".menu-burger")) {
-		if (body.classList.contains("menu-open")) {
-			body.classList.remove("menu-open");
-			body.classList.add("menu-close");
-		} else if (body.classList.contains("menu-close")) {
-			body.classList.remove("menu-close");
-			body.classList.add("menu-open");
-		} else {
-			body.classList.add("menu-open");
-		}
+		const isOpen = body.classList.contains("menu-open");
+
+		body.classList.remove(isOpen ? "menu-open" : "menu-close");
+		body.classList.add(isOpen ? "menu-close" : "menu-open");
 	}
 });
